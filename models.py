24bbd127d09e585a68c00241341603a278d117b1
benzine12 <176717159+benzine12@users.noreply.github.com>
2025-01-15 15:38:54 +0200

    bugfix

--- models.py
+++ models.py
@@ -5,13 +5,6 @@ db = SQLAlchemy()
 class Task(db.Model):
     __tablename__ = 'tasks'
 
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Task(db.Model):
-    __tablename__ = 'tasks'
-
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(60), nullable=False)
     theme = db.Column(db.String(10), nullable=False)
