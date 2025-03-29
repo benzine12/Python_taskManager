8c1a307a0f4027aa885154575ca353e21bf35872
benzine12 <176717159+benzine12@users.noreply.github.com>
2025-03-29 18:01:03 +0300

    updated test_login to include full test of login function

--- Backend/tests/test_login.py
+++ Backend/tests/test_login.py
@@ -1,10 +1,43 @@
 # test_login.py
-from flask_jwt_extended import create_access_token
 
-def test_login(client):
-    response = client.post('/login', # Simulates POST request to /register
-    json={"username":"test_username","password":"test_password"})
+def test_login(client,test_user):
+    # Good case
+    response = client.post('/login',
+                          json={"username": test_user["username"], 
+                                "password": test_user["password"]})
 
-    assert response.status_code == 201  # Check if status code is 201
-    # assert response.json == {"msg": "Welcome back, commander!",
-    #                          "access_token": create_access_token(identity=str('test_username'))}
+    assert response.status_code == 200 
+    assert response.json["msg"] == "Welcome back, commander!"
+    assert "access_token" in response.json
+
+def test_login_invalid_method(client,test_user):
+    response = client.get('/login',
+                          json={"username": test_user["username"], 
+                                "password": test_user["password"]})
+    
+    assert response.status_code == 405
+
+def test_login_invalid_request(client):
+    response = client.post('/login')
+
+    assert response.status_code == 400
+    assert response.json == {"msg": "Missing or invalid JSON in request",
+                            "error": "Bad request"}
+
+def test_login_missing_credits(client,test_user):
+    response = client.post('/login',
+                           json={"username":test_user["username"]})
+    
+    assert response.status_code == 400
+    assert response.json == {"msg": "Username and password are required",
+                            "error": "Bad request"}
+    
+def test_login_invalid_credits(client,test_user):
+        response = client.post('/login',
+                          json={"username": "username123", 
+                                "password": "username123"})
+        
+        assert response.status_code == 401
+        assert response.json == {"msg": "Invalid username or password",
+                        "error": "Something went wrong"}
+        
